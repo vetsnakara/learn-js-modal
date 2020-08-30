4827833106221bbf9e4ef18f877b5e2da6a1dc59
Konstantin Arakantsev <kostya3312@gmail.com>
2020-08-30 14:02:45 +0300

    gsap wave hand animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+import gsap, { Back } from 'gsap'
+
 const modalButton = document.querySelector('.jsModalButton')
 const modalCloseButton = document.querySelector('.jsModalClose')
 const modalOverlay = document.querySelector('.jsModalOverlay')
+const waveHand = document.querySelector('.wave-hand')
 
 const MODAL_CLOSE_CLASS = 'modal-is-open'
+
+const wave = () => {
+  const tl = gsap.timeline()
+
+  tl.from(waveHand, 0.5, {
+    scale: 0.25,
+    opacity: 0,
+    ease: Back.easeOut.config(1.5)
+  })
+
+  tl.to(waveHand, 0.2, { rotation: '15deg' })
+  tl.to(waveHand, 0.2, { rotation: '-15deg' })
+  tl.to(waveHand, 0.2, { rotation: '15deg' })
+  tl.to(waveHand, 0.2, { rotation: '-15deg' })
+  tl.to(waveHand, 0.2, { rotation: '0deg' })
+}
 
 // open modal
 modalButton.addEventListener('click', () => {
   document.body.classList.add(MODAL_CLOSE_CLASS)
+  wave()
 })
 
 // close modal (click on close button)
