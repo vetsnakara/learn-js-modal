3b142179389b6a0e94f10a55c63dc84cd96e1f6c
Konstantin Arakantsev <kostya3312@gmail.com>
2020-08-30 10:46:47 +0300

    close modal on click outside

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const modalButton = document.querySelector('.jsModalButton')
 const modalCloseButton = document.querySelector('.jsModalClose')
+const modalOverlay = document.querySelector('.jsModalOverlay')
 
 const MODAL_CLOSE_CLASS = 'modal-is-open'
 
+// open modal
 modalButton.addEventListener('click', () => {
   document.body.classList.add(MODAL_CLOSE_CLASS)
 })
 
+// close modal (click on close button)
 modalCloseButton.addEventListener('click', () => {
   document.body.classList.remove(MODAL_CLOSE_CLASS)
+})
+
+// close modal (click outside)
+modalOverlay.addEventListener('click', e => {
+  if (!e.target.closest('.modal')) {
+    document.body.classList.remove(MODAL_CLOSE_CLASS)
+  }
 })
